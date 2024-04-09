0a4319b7dea365cb2ac60dd6485340e571c08493
alvishui <alvishui345@gmail.com>
2024-04-09 13:35:51 -0400

    Setup of the project

---
+++ src/main/java/net/alvisssss/advancedmissile/AdvancedMissileClient.java
@@ -0,0 +1,10 @@
+package net.alvisssss.advancedmissile;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AdvancedMissileClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
